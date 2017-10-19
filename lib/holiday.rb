58c03bb11494231e28c7745b3753f309933015b1
Julianne Kim <juliannne@gmail.com>
2017-10-19 18:53:26 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     data.each do |holiday, supply|
       puts "#{season.capitalize}:"
-      puts "#{holiday.capitalize}: #{supply}"
+      puts "#{holiday.capitalize}: #{supply.join(", ")}"
     end
   end
 end
