3363f1a9416207d041d825ed3dcc43145526d8ab
Julianne Kim <juliannne@gmail.com>
2017-10-19 19:04:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,11 +59,12 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, data|
     data.each do |holiday, supply|
-      puts "#{season.capitalize}:"
       mod_holiday = holiday.to_s.split("_").map do |word|
         word.capitalize
       end
-      puts "  #{mod_holiday.join(" ")}: #{supply.join(", ")}"
+      mod_holiday = mod_holiday.join(" ")
+      puts "#{season.capitalize}:"  
+      puts "  #{mod_holiday}: #{supply.join(", ")}"
     end
   end
 end
